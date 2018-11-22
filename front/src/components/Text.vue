ad1e662149144f20477739797d1c308e1c245321
Maxim Tishchenko <qwertmax@gmail.com>
2018-11-22 08:31:09 +0300

    text

--- front/src/components/Text.vue
+++ front/src/components/Text.vue
@@ -1,10 +1,15 @@
 <template>
   <form>
-    <span><input type="text" name="text" placeholder="Text"></span>
+    <span><input type="text" name="text" placeholder="Text" @change="changeVal" /></span>
   </form>
 </template>
 
 <script>
 export default {
+  methods: {
+    changeVal: function (e) {
+      this.$bus.$emit('makeQREvent', e.target.value)
+    }
+  }
 }
 </script>
